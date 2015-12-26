10b95d9cba4886441629aa8c3b2555200fdec6c6
Tony Chung <anchung@gmail.com>
2015-12-26 14:57:12 -0500

    remove console log

--- exp/app.js
+++ exp/app.js
@@ -22,9 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(credentials.cookieSecret)); //credentials.cookieSecret
 app.use(express.static(path.join(__dirname, 'public')));
-
-console.log('IN APP JS');
-console.log(path.resolve(__dirname,'..', 'reactapp/dist'));
 app.use(express.static(path.resolve(__dirname,'..', 'app/dist')));
 
 //app.use('/', routes);
